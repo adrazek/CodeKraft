b89edef16fab52c756c5f86e3fe430720a37f60f
nO <adrazek@gmail.com>
2017-01-24 12:48:02 +0000

    Finalizing integration + contents

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,27 @@
 class WelcomeController < ApplicationController
   def index
+
+    @technos = [
+      {
+        type: "Langages", 
+        values: ["css3", "html5", "php", "python", "javascript", "jquery", "react"]
+      },
+      {
+        type: "Frameworks",
+        values: ["rails", "nodejs", "symfony", "zend"]
+      },
+      {
+        type: "Base de données",
+        values: ["mysql", "mongodb", "postgresql", "neo4j"],
+      },
+      {
+        type: "Système", 
+        values: ["linux", "apache", "nginx"]
+      },
+      {
+        type: "Outils",
+        values: ["github", "trello", "bitbucket"],
+      },
+    ]
   end
 end
