5173ee56f4cb15178ab4ce64f51fa1740328bc35
nO <adrazek@gmail.com>
2017-01-27 13:32:50 +0000

    Adding slack notification from contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,17 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:contact_send] = true
+
+      #if Rails.env.production?
+
+        notifier = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+          defaults channel: "#contacts",
+                   username: "facteur"
+        end
+
+        notifier.ping "\nNew message from *#{contact_params[:name]}* (<#{contact_params[:email]}>)\n\n_#{contact_params[:message]}_\n"
+      #end
+
     else
       flash[:contact_send] = false
     end
